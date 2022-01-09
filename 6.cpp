1bcd934b37294bc65db875e5206da0299ba87744
Kishor Thagunna <thaunnakishor@gmail.com>
2022-01-09 22:30:04 +0545

    Correction

--- 6.cpp
+++ 6.cpp
@@ -10,24 +10,69 @@
 // Use proper default argument.
 #include<iostream>
 using namespace std;
-struct salary
-{
-    
-}sa;
-
-float increment(float,float);
-float total_salary(float,float);
+inline float increment(float,float);
+inline float total_salary(float,float);
 int main()
 {
-    int CEO=35000,IO=25000,SA=24000,PG=18000;
-    int c=9,i=10,s=12,p=12;
-    float C,I,S,P,ceo,io,sa,pg;
+    float CEO,IO,SA,PG,c,i,s,p;
+    float C,I,S,P;
+    cout<<"Enter the salary of Chief executive officer and increment in percentage (per year):";
+    cin>>CEO>>c;
+    cout<<"Enter the salary of Information officer and increment in percentage (per year):";
+    cin>>IO>>i;
+    cout<<"Enter the salary of System analyst and increment in percentage (per year):";
+    cin>>SA>>s;
+    cout<<"Enter the salary of Programmer and increment in percentage (per year):";
+    cin>>PG>>p;  
     C=increment(CEO,c);
     I=increment(IO,i);
     S=increment(SA,s);
     P=increment(PG,p);
-    ceo=total_salary(CEO,C);
-    io=total_salary(IO,I);
-    
+    cout<<" \nSalary of year 2009."<<endl<<"--------------------------";
+    cout<<"  \nChief executive officer :"<<CEO;
+    cout<<"  \nInformation officer :"<<IO;
+    cout<<"  \nSystem Analyst :"<<SA;
+    cout<<"  \nProgrammer :"<<PG;
+    cout<<" \n\n\n\nIncrement of year 2009."<<endl<<"--------------------------";
+    cout<<"  \nChief executive officer :"<<C<<" BY "<<c<<" Percent";
+    cout<<"  \nInformation officer :"<<I<<" BY "<<i<<" Percent";
+    cout<<"  \nSystem Analyst :"<<S<<" BY "<<s<<" Percent";
+    cout<<"  \nProgrammer :"<<P<<" BY "<<p<<" Percent";
+    cout<<" \n\n\n\nNet for year 2010."<<endl<<"--------------------------";
+    cout<<"  \nChief executive officer :"<<total_salary(CEO,C);
+    cout<<"  \nInformation officer :"<<total_salary(IO,I);
+    cout<<"  \nSystem Analyst :"<<total_salary(SA,S);
+    cout<<"  \nProgrammer :"<<total_salary(PG,P);
     return 0;
+}
+float increment(float X,float Y)
+{
+    return (X/100)*10 ;
+}
+float total_salary(float a,float b)
+{
+    return a+b ;
+}
+
+// OR----- Simple
+
+#include<iostream>
+
+using namespace std;
+int incsal(int salary,int rate);
+int main()
+{
+  cout<<"The incremented salary of cheif executive officier is :"<<incsal(35000,9)<<endl;
+
+  cout<<"The incremented salary of information officier is :"<<incsal(25000,10)<<endl;
+
+  cout<<"The incremented salary of system analyst is :"<<incsal(24000,12)<<endl;
+
+  cout<<"The incremented salary of programmer is :"<<incsal(18000,12)<<endl;
+
+  return 0;  
+}
+int incsal(int salary,int rate)
+{
+  return (salary+((rate*salary)/100));
 }
